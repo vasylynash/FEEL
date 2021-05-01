848a67e3b36f085d2a0e46c164ab533b446f0f3c
Vasylyna Shevadutska <vaskash@gmail.com>
2021-05-01 13:26:37 -0700

    Added values to html dropdowns; changed script.js to dynamically generate link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var searchKeyword = $("#keyword");
-var cuisine = $("#cuisine");
-var meal = $("#mealType");
-var diet = $("#diet");
+var cuisineSel = $("#cuisine");
+var mealSel = $("#mealType");
+var dietSel = $("#diet");
 var submitBtn = $("#submit");
 var searchResults = $("#search-results");
 
@@ -11,19 +11,35 @@ const requestURL = "https://api.edamam.com/search";
 
 
 function showRecipes() {
-    var keyword = getUserInput();
+    var searchOptions = getUserInput();
     const parsedResponsePromise = recipePromise(keyword)
         .then(parseToJson)
         .then(renderSearchResults)
 }
 
 function getUserInput() {
-    var keyword = searchKeyword.val().trim();
-    return keyword;
+    var searchOptions = {
+        q: "",
+        cuisineType: "",
+        mealType: "",
+        diet: "",
+
+    }
+    searchOptions.q = searchKeyword.val().trim();
+    searchOptions.cuisineType = cuisineSel.val();
+    searchOptions.mealType = mealSel.val();
+    searchOptions.diet = dietSel.val();
+    console.log(searchOptions);
+    return searchOptions;
 }
 
-function recipePromise(keyword) {
-    var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+function recipePromise(searchOptions) {
+    var queryString = Object.keys(searchOptions).map(key => key + "=" + searchOptions[key]).join("&");
+    // var queryString = $.param(searchOptions);
+    console.log(queryString);
+    var url = requestURL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    console.log(url);
+    // var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
     return fetch(url);
 }
 
@@ -39,8 +55,15 @@ function renderSearchResults(data) {
         const recipe = recipes[i];
         var name = recipe.recipe.label;
         console.log(name);
-        var listItem = (`<div name="list-item-${i}"><h3>${name}</h3></div>`)
+        var listItem = (`<div name="list-item-${i}"><h3>${name}</h3></div>`);
+        var buttonUrl = recipe.recipe.url;
+        var instructionsButton = (`<a href="${buttonUrl}">How to</button>`)
+        var infoButton = (`<a href="${buttonUrl}">More info</button>`)
+        var videosButton = (`<a href="${buttonUrl}">Find videos</button>`)
         searchResults.append(listItem);
+        searchResults.append(infoButton);
+        searchResults.append(instructionsButton);
+        searchResults.append(videosButton);
     }
 }
 
