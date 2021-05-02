e560fccf72c9fbea4de22c2d97f2a45bb843ba97
Vasylyna Shevadutska <vaskash@gmail.com>
2021-05-01 17:01:04 -0700

    Reformat script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,54 +11,61 @@ const requestURL = "https://api.edamam.com/search";
 
 
 function showRecipes() {
-    var searchOptions = getUserInput();
-    var queryString = $.param(searchOptions)
+    var queryString = buildQueryString();
     var url = requestURL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const parsedResponsePromise = recipePromise(searchOptions)
+    fetch(url)
         .then(parseToJson)
         .then(renderSearchResults)
+        .catch(function (errorMessage) {
+            if (typeof errorMessage !== "string") {
+                errorMessage = "Can't connect to server";
+            }
+            searchResults.empty();
+            var error = $(`<div class="error"><h3>${errorMessage}</3></div>`)
+            searchResults.append(error);
+        })
 }
 
-function getUserInput() {
+function buildQueryString() {
     var searchOptions = {
-        q: "",
-        cuisineType: "",
-        mealType: "",
-    }
-    searchOptions.q = searchKeyword.val().trim();
-    searchOptions.cuisineType = cuisineSel.val();
-    searchOptions.mealType = mealSel.val();
-    return searchOptions;
-}
-
-function recipePromise(searchOptions) {
-    // console.log(searchOptions);
-    var queryString = Object.keys(searchOptions).map(key => key + "=" + searchOptions[key]).join("&");
-    // var queryString = $.param(searchOptions);
-    console.log(queryString);
-    var url = requestURL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    console.log(url);
-    // var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    return fetch(url);
+        q: searchKeyword.val().trim(),
+        cuisineType: cuisineSel.val(),
+        mealType: mealSel.val(),
+    };
+    $.each(searchOptions, function (key, value) {
+        if (value === "") {
+            delete searchOptions[key];
+        }
+    });
+    var queryString = $.param(searchOptions);
+    return queryString;
 }
 
 //TODO move to helpers.js
 function parseToJson(response) {
+    if (!response.ok) {
+        throw "Can't retrieve data";
+    }
     return response.json();
 }
 
 function renderSearchResults(data) {
+
     var recipes = data.hits;
     console.log(recipes);
+    if (!recipes.length) {
+        throw "No results found";
+    }
+    searchResults.empty();
     for (let i = 0; i < recipes.length; i++) {
         const recipe = recipes[i];
         var name = recipe.recipe.label;
         console.log(name);
         var listItem = (`<div name="list-item-${i}"><h3>${name}</h3></div>`);
         var buttonUrl = recipe.recipe.url;
-        var instructionsButton = (`<a href="${buttonUrl}">How to</button>`)
-        var infoButton = (`<a href="${buttonUrl}">More info</button>`)
-        var videosButton = (`<a href="${buttonUrl}">Find videos</button>`)
+        var instructionsButton = (`<a href="${buttonUrl}">How to</button>`);
+        var infoButton = (`<a href="${buttonUrl}">More info</button>`);
+        var videosButton = (`<a href="${buttonUrl}">Find videos</button>`);
         searchResults.append(listItem);
         searchResults.append(infoButton);
         searchResults.append(instructionsButton);
@@ -68,6 +75,5 @@ function renderSearchResults(data) {
 
 submitBtn.on("click", function (event) {
     event.preventDefault();
-    getUserInput();
     showRecipes();
 });
