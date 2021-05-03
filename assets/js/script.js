baf1e5f967464be4dc357a49d34f9900cbf3ee29
eweagraff <weagraff.emily@gmail.com>
2021-05-03 13:25:47 -0700

    created back button to search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,43 +9,47 @@ const APP_KEY = "4147e89469febd4e4e9264cc0a6e7cbe";
 const APP_ID = "c36e8feb";
 const requestURL = "https://api.edamam.com/search";
 
-
 function showRecipes() {
-    var keyword = getUserInput();
-    const parsedResponsePromise = recipePromise(keyword)
-        .then(parseToJson)
-        .then(renderSearchResults)
+  var keyword = getUserInput();
+  const parsedResponsePromise = recipePromise(keyword)
+    .then(parseToJson)
+    .then(renderSearchResults);
 }
 
 function getUserInput() {
-    var keyword = searchKeyword.val().trim();
-    return keyword;
+  var keyword = searchKeyword.val().trim();
+  return keyword;
 }
 
 function recipePromise(keyword) {
-    var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    return fetch(url);
+  var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  return fetch(url);
 }
 
 //TODO move to helpers.js
 function parseToJson(response) {
-    return response.json();
+  return response.json();
 }
 
 function renderSearchResults(data) {
-    var recipes = data.hits;
-    console.log(recipes);
-    for (let i = 0; i < recipes.length; i++) {
-        const recipe = recipes[i];
-        var name = recipe.recipe.label;
-        console.log(name);
-        var listItem = (`<div name="list-item-${i}"><h3>${name}</h3></div>`)
-        searchResults.append(listItem);
-    }
+  var recipes = data.hits;
+  console.log(recipes);
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    var name = recipe.recipe.label;
+    console.log(name);
+    var listItem = `<div name="list-item-${i}"><h3>${name}</h3></div>`;
+    searchResults.append(listItem);
+  }
 }
 
 submitBtn.on("click", function (event) {
-    event.preventDefault();
-    getUserInput();
-    showRecipes();
+  event.preventDefault();
+  getUserInput();
+  showRecipes();
 });
+
+//youTube API
+
+const youTubeUrl =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=suggestions&part=topicDetails&maxResults=20&myRating=like&key=[YOUR_API_KEY]";
