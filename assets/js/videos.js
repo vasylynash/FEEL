4fabb3c8e9059001712776afea50d2a85de2f1ec
Vasylyna Shevadutska <vaskash@gmail.com>
2021-05-04 13:50:54 -0700

    Added modal logic to script.js

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,2 +1,2 @@
-const youTubeApi = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=${AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc}`;
-const apiKey = "AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc";
+// const youTubeApi = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=${AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc}`;
+// const apiKey = "AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc";
