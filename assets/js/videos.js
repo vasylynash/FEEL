3f40d4929c00baca98571fb7508cdae1c5a15167
eweagraff <weagraff.emily@gmail.com>
2021-05-04 16:32:45 -0700

    building youtube video javascript page

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,2 +1,21 @@
-const youTubeApi = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=${AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc}`;
+var searchEl = $(`#search`);
+var resultsEl = $(`#results`);
+var submitBtn = $(`#submit`);
+
+const youTubeUrl = `https://youtube.googleapis.com/youtube/v3/search`;
 const apiKey = "AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc";
+
+$(function () {
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+    var queryString = buildQueryString();
+    var requestUrl = youTubeUrl + "?" + queryString + `&app_key=${apiKey}`;
+    fetch(requestUrl).then(parseToJson);
+  });
+});
+
+function buildQueryString() {
+  var searchOptions = {
+    q: searchEl.val().trim(),
+  };
+}
