9e7b13ad9ef6e2cf01b10a87b6501feb5c62e715
Vasylyna Shevadutska <vaskash@gmail.com>
2021-05-03 18:47:35 -0700

    Shevadutska (#11)
    * Reformat script.js
    
    * Added pagination; changed the search results' generation to use Semantic UI components

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,69 +5,124 @@ var dietSel = $("#diet");
 var submitBtn = $("#submit");
 var searchResults = $("#search-results");
 
+var currentPage = 0;
+
 const APP_KEY = "4147e89469febd4e4e9264cc0a6e7cbe";
 const APP_ID = "c36e8feb";
-const requestURL = "https://api.edamam.com/search";
+const REQUEST_URL = "https://api.edamam.com/search";
+const PAGE_SIZE = 20;
 
 
 function showRecipes() {
-    var searchOptions = getUserInput();
-    var queryString = $.param(searchOptions)
-    var url = requestURL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    const parsedResponsePromise = recipePromise(searchOptions)
+    var queryString = buildQueryString();
+    var url = REQUEST_URL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}&from=${PAGE_SIZE * currentPage}&to=${PAGE_SIZE * currentPage + PAGE_SIZE}`;
+    fetch(url)
         .then(parseToJson)
         .then(renderSearchResults)
+        .catch(function (errorMessage) {
+            if (typeof errorMessage !== "string") {
+                errorMessage = "Can't connect to server";
+            }
+            searchResults.empty();
+            var error = $(`<div class="error"><h3>${errorMessage}</3></div>`)
+            searchResults.append(error);
+        })
 }
 
-function getUserInput() {
+function buildQueryString() {
     var searchOptions = {
-        q: "",
-        cuisineType: "",
-        mealType: "",
-    }
-    searchOptions.q = searchKeyword.val().trim();
-    searchOptions.cuisineType = cuisineSel.val();
-    searchOptions.mealType = mealSel.val();
-    return searchOptions;
-}
-
-function recipePromise(searchOptions) {
-    // console.log(searchOptions);
-    var queryString = Object.keys(searchOptions).map(key => key + "=" + searchOptions[key]).join("&");
-    // var queryString = $.param(searchOptions);
-    console.log(queryString);
-    var url = requestURL + "?" + queryString + `&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    console.log(url);
-    // var url = requestURL + `?q=${keyword}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    return fetch(url);
+        q: searchKeyword.val().trim(),
+        cuisineType: cuisineSel.val(),
+        mealType: mealSel.val(),
+    };
+    $.each(searchOptions, function (key, value) {
+        if (value === "") {
+            delete searchOptions[key];
+        }
+    });
+    var queryString = $.param(searchOptions);
+    return queryString;
 }
 
 //TODO move to helpers.js
 function parseToJson(response) {
+    if (!response.ok) {
+        throw "Can't retrieve data";
+    }
     return response.json();
 }
 
 function renderSearchResults(data) {
+
     var recipes = data.hits;
     console.log(recipes);
+    if (!recipes.length) {
+        throw "No results found";
+    }
+    searchResults.empty();
     for (let i = 0; i < recipes.length; i++) {
         const recipe = recipes[i];
         var name = recipe.recipe.label;
-        console.log(name);
-        var listItem = (`<div name="list-item-${i}"><h3>${name}</h3></div>`);
-        var buttonUrl = recipe.recipe.url;
-        var instructionsButton = (`<a href="${buttonUrl}">How to</button>`)
-        var infoButton = (`<a href="${buttonUrl}">More info</button>`)
-        var videosButton = (`<a href="${buttonUrl}">Find videos</button>`)
-        searchResults.append(listItem);
-        searchResults.append(infoButton);
-        searchResults.append(instructionsButton);
-        searchResults.append(videosButton);
+        var time = recipe.recipe.totalTime;
+        var calories = recipe.recipe.calories;
+        var url = recipe.recipe.url;
+
+        var resultSegment = $(`<div class="ui vertical segment"></div>`);
+        var resultBody = $(`<div>`);
+        var title = $(`<h3><a class="btn btn-link" href="${url}">${name}</a></h3>`);
+        var bodyContentTime = $(`<p>Time to cook: ${time}</p>`);
+        var bodyContentCalories = $(`<p>Calories: ${Math.floor(calories)}</p>`);
+
+        var instructionsButton = (`<div class="ui animated purple button" tabindex="0">
+                    <div class="visible content">Ingredients</div>
+                        <div class="hidden content">
+                        <i class="right arrow icon"></i>
+                        </div>
+                    </div>
+                    </div>`);
+        var videosButton = (`<div class="ui animated youtube button" tabindex="0">
+                    <div class="visible content">Find videos</div>
+                        <div class="hidden content">
+                        <i class="youtube icon"></i>
+                        </div>
+                    </div>
+                    </div>`);
+        resultBody.append(title);
+        resultBody.append(bodyContentTime);
+        resultBody.append(bodyContentCalories);
+        resultBody.append(instructionsButton);
+        resultBody.append(videosButton);
+        resultSegment.append(resultBody);
+        searchResults.append(resultSegment);
+    }
+    var count = data.count;
+    var isLastPage = ((currentPage + 1) * PAGE_SIZE) >= count;
+    var isFirstPage = (currentPage === 0);
+
+    var previousButton = $(`<button class="left attached ui button" id="previous">Previous</button>`);
+    var nextButton = $(`<button class="left attached ui button" id="next">Next</button>`);
+    searchResults.append(previousButton);
+    if (isFirstPage) {
+        previousButton.addClass("disabled");
+    }
+    searchResults.append(nextButton);
+    if (isLastPage) {
+        nextButton.addClass("disabled");
     }
 }
 
 submitBtn.on("click", function (event) {
     event.preventDefault();
-    getUserInput();
+    currentPage = 0;
+    showRecipes();
+});
+
+searchResults.on("click", "#next", function () {
+    currentPage++;
+    showRecipes();
+});
+
+searchResults.on("click", "#previous", function () {
+    currentPage--;
     showRecipes();
 });
