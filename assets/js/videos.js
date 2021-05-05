6d5bac46aebd59e15caf59e36dd7d0c0c14cf2de
eweagraff <weagraff.emily@gmail.com>
2021-05-04 19:55:08 -0700

    removed bad code in js file

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,6 +1,7 @@
-var searchEl = $(`#search`);
 var resultsEl = $(`#results`);
-var submitBtn = $(`#submit`);
+var resultsTextEl = $(`result-text`);
+
+function getParams() {}
 
 const youTubeUrl = `https://youtube.googleapis.com/youtube/v3/search`;
 const apiKey = "AIzaSyA3rkHsQ8EWGM113wT7U7Ie_BV6LG3g3oc";
